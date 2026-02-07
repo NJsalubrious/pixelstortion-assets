f726c6cf797cb48a184498236e01180a80072e9c
NJsalubrious <40312670+NJsalubrious@users.noreply.github.com>
2026-02-07 19:20:45 +1100

    image lobrary links ["../../library/media_covers_album/"]

--- zones/ethel_gallery/ethel_lyrics_data.js
+++ zones/ethel_gallery/ethel_lyrics_data.js
@@ -5,7 +5,7 @@
  */
 
 // Using main branch = auto-updates when you push to git
-const IMG_BASE = "https://raw.githubusercontent.com/NJsalubrious/pixelstortion-assets/library/media_covers_album/";
+const IMG_BASE = "../../library/media_covers_album/";
 
 const ARCHIVE_DATA = [
     // =========================================================================
