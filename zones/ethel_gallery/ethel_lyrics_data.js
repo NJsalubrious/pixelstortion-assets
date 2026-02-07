9a39f86db575b64574941af8cbfb3d76167db9c4
NJsalubrious <40312670+NJsalubrious@users.noreply.github.com>
2026-02-07 19:32:15 +1100

    lyrics

--- zones/ethel_gallery/ethel_lyrics_data.js
+++ zones/ethel_gallery/ethel_lyrics_data.js
@@ -5,7 +5,7 @@
  */
 
 // Using main branch = auto-updates when you push to git
-const IMG_BASE = "../../library/media_covers_album/";
+const IMG_BASE = "../../library/media_covers_song/";
 
 const ARCHIVE_DATA = [
     // =========================================================================
