40766a7fa94a57c9ce827250a709bd78246dc768
NJsalubrious <40312670+NJsalubrious@users.noreply.github.com>
2026-02-09 23:32:34 +1100

    new additions

--- site-manifest.js
+++ site-manifest.js
@@ -78,7 +78,7 @@ const PIXEL_MANIFEST = {
             fullscreen: false
         },
         DOMINIC_2: {
-            url: '/zones/games/ryker_consensus.html',
+            url: '/zones/games/RykerGame_DeletedPicsAudioSatellites.html',
             name: 'Forced Network Compliance V1.0',
             thumb: '/library/silence_games/Dominic_game_3.jpg',
             fullscreen: true
